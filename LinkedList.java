b2f4fb7fa907a26bd060549c874e1f75c3f9b7b1
Ashvini Deokate <89019792+AshviniDeo@users.noreply.github.com>
2021-09-26 05:18:43 +0530

    Ability to add and print

--- LinkedList.java
+++ LinkedList.java
@@ -2,11 +2,11 @@ package bridgeLabz.linkedList;
 
 class Node{
 	int data;
+	Node nextPointer;
 	@Override
 	public String toString() {
 		return "Node [data=" + data + ", nextPointer=" + nextPointer + "]";
 	}
-	Node nextPointer;
 	public Node(int data) {
 		this.data = data;
 		this.nextPointer = null;
@@ -15,6 +15,30 @@ class Node{
 
 public class LinkedList {
 
- 
-	
+	static Node head;
+
+	public void addNode(int data) {
+
+		Node node = new Node(data);
+		if(head == null) {
+			head = node;
+		}else {
+			Node temp = head;
+			while(temp.nextPointer != null) {
+				temp = temp.nextPointer;
+			}
+			temp.nextPointer = node;
+		}
+
+	}
+
+	public void displayNode() {
+		Node temp = head;
+		while(temp != null) {
+			System.out.print(temp.data + " -> ");
+			temp = temp.nextPointer;
+		}
+
+	}
+
 }
