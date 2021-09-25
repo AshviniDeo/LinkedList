428d9970ca6af6b7f4bd764500b17495188b413a
Ashvini Deokate <89019792+AshviniDeo@users.noreply.github.com>
2021-09-26 04:57:41 +0530

    Simple Linked List created

---
+++ LinkedList.java
@@ -0,0 +1,20 @@
+package bridgeLabz.linkedList;
+
+class Node{
+	int data;
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", nextPointer=" + nextPointer + "]";
+	}
+	Node nextPointer;
+	public Node(int data) {
+		this.data = data;
+		this.nextPointer = null;
+	}
+}
+
+public class LinkedList {
+
+ 
+	
+}
